89d7a4fcbc0898a028cf03f2b4f192e64f33a4de
Tickether <tickether@gmail.com>
2023-11-15 10:01:22 +0000

    lazy ts fix: cc biconomy

--- utils/ethers/provider.ts
+++ utils/ethers/provider.ts
@@ -2,6 +2,7 @@ import { magic } from "../magic/magicProvider"
 import { ethers } from "ethers"
 
 export const provider =  new ethers.providers.Web3Provider(
+    //@ts-ignore
     magic.rpcProvider,
     "any"
 );
